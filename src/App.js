902dd03d6fca3cb6f1ce043140bac87940b6dbc5
RahulJindal1236 <rahuljindal1236@gmail.com>
2021-08-11 10:54:11 +0530

    Action Added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>This is App Component</h1>
+      <div>
+        <h1>Redux Example</h1>
+        <p>
+          This is a simple redux example using actions, reducers,store and
+          dispatcher
+        </p>
+      </div>
+      <div>
+        <button>-</button>
+        <input type='text' value='0' />
+        <button>+</button>
+      </div>
     </div>
   )
 }
